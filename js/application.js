9ebf78e0f05e94268c66eaf1946c74a58f4065a3
Analita <88949609+warlita@users.noreply.github.com>
2024-03-04 21:12:44 -0600

    complete change of project

--- js/application.js
+++ js/application.js
@@ -1,87 +1,54 @@
-
-//add to table
-$(document).ready(function () {
-  $('body').on("click", "#addToCart", function (event) {
-    //prevent default is added so when the button is clicked our page is NOT reloading
-    event.preventDefault();
-
-    //housing our data obtained from user into new variables
-    const item = document.querySelector('.newItem').value;
-    const price = document.querySelector('.newPrice').value;
-    const quantity = document.querySelector('.newQuantity').value;
-
-    //create a new row for the table
-    const newRow = document.createElement('tr');
-
-    //this is the information that will go into the new row
-    newRow.innerHTML = `
-    <td class="items">${item}</td>
-    <td class="prices">${price}</td>
-    <td class="quantity">${quantity}</td>
-    <td class="totalprice"></td>
-    <td class="deleteItem">
-    <button class="btn btn-danger">Delete</button>
-    </td>
-    `;
-    //append the new row to the table
-    document.querySelector('table').appendChild(newRow);
-  });
-  //clear the input fields for the user to enter new item
-  document.querySelector(".newItem").value = "";
-  document.querySelector(".newPrice").value = "";
-  document.querySelector(".newQuantity").value = "";
-})
-
-$(document).ready(function () {
-  //calculating subtotal
-  var calculateSubTotal = function (ele) {
-    var quantityItem = Number(`${$(ele).find(".quantity").val()}` || `${$(ele).find(".newQuantity input").val()}`);
-    var priceItem = Number(`${$(ele).children(".prices").text().replace(/[^0-9.]+/g, "")}` || `${$(ele).find(".newPrices input").text().replace(/[^0-9.]+/g, "")}`);
-
-    var subTotal = quantityItem * priceItem;
-
-    if (subTotal >= 0) {
-      $(ele)
-        .children(".totalprice")
-        .html(`$${parseFloat(Math.round(subTotal * 100) / 100).toFixed(2)}`);
-    }
-
-    return subTotal;
-  };
-
-  //updating Item Price or (subtotal) 
-  var total = 0;
-
-  var updateItemPrice = function () {
-    var allItemPrices = [];
-
-    $("tbody tr").each(function (i, ele) {
-      var subTotal = calculateSubTotal(this);
-      allItemPrices.push(subTotal);
-    });
-
-    if (allItemPrices.length == 0) {
-      $("#finalPrice").html(`$--.--`);
+// calculate cost per item & total
+var updateCost = function() {
+  var costArr = [];
+  $('tbody tr').each(function (i, item) {
+    var linePrice = parseFloat($(item).find('.price').text());
+    var quantity = parseFloat($(item).find('.qty input').val());
+    var itemCost = linePrice * quantity;
+    if(quantity) {
+      $(item).children('.cost').html(itemCost.toFixed(2));
+      costArr.push(itemCost);
     } else {
-      var totalCart = allItemPrices.reduce(function (acc, x) {
-        return acc + x;
-      });
-      $("#finalPrice").html(totalCart);
-    };
-  };
-
-  //update shopping cart
-  var timeout;
-  $("#item-list").on("change", ".quantity input", function () {
-    console.log('qty input')
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      updateItemPrice();
-    }, 500);
+      $(item).children('.cost').html('');
+    }
   });
+  var total = costArr.length > 0 ? costArr.reduce((sum, num) => sum + num) : 0;
+  $('#grandTotal').html(total.toFixed(2)); 
+};
 
-  // remove button
-  $("body").on("click", ".deleteItem", function (event) {
-    $(this).closest("tr").remove();
-  })
+// add item to cart
+var addItem = function() {
+  var newItem = $('#item').val();
+  var newPrice = parseFloat($('#price').val()).toFixed(2);
+  if (!newItem || isNaN(newPrice)) {
+    alert('You must enter both item name and unit price to add a new item.');
+  } else {
+    $('#lastRow').before("<tr><td class='item'>" + newItem + "</td><td class='price'>" + newPrice + "</td><td class='qty'><input type='number'></input></td><td class='cost'></td><td><button class='btn btn-danger remove'>Delete</button></td></tr>");  
+  }
+  $('tr').find('#item, #price').val('');
+};
+
+// remove item from cart
+var removeItem = function() {
+  $(this).closest('tr').remove();
+  updateCost();
+};
+
+// update cart when quantities change
+var updateQty = function () {
+  clearTimeout(delay);
+  var delay = setTimeout(updateCost, 500);
+};
+
+// event handlers
+$(document).ready(function() {
+  updateCost();
+  $(document).on('input', '.qty', updateQty);
+  $(document).on('click', '.remove', removeItem);
+  $(document).on('click', '.add', addItem);
+  $('#price').on('keyup', function(event) {
+    if (event.key === 'Enter') {
+      addItem();
+    }
+  });
 });
